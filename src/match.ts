bb1a4b8b34a1dee8fd61f7b0824610607e31c36a
undg <und3rdg@gmail.com>
2024-11-02 14:20:02 +0000

    actually native js private instad just transpile fake private

--- src/match.ts
+++ src/match.ts
@@ -11,11 +11,11 @@ type Teams = [
 ];
 
 export class Match {
-	private startTime: Date;
-	private homeTeam = "";
-	private homeScore = 0;
-	private awayTeam = "";
-	private awayScore = 0;
+	#startTime: Date;
+	#homeTeam = "";
+	#homeScore = 0;
+	#awayTeam = "";
+	#awayScore = 0;
 
 	/**
 	 * Creates a new Match instance.
@@ -24,25 +24,25 @@ export class Match {
 	constructor(teams: Teams) {
 		const home = teams[0];
 		const away = teams[1];
-		this.startTime = new Date();
-		this.homeTeam = home.name;
-		this.homeScore = home.score ?? this.homeScore;
-		this.awayTeam = away.name;
-		this.awayScore = away.score ?? this.awayScore;
+		this.#startTime = new Date();
+		this.#homeTeam = home.name;
+		this.#homeScore = home.score ?? this.#homeScore;
+		this.#awayTeam = away.name;
+		this.#awayScore = away.score ?? this.#awayScore;
 	}
 
 	update(homeScore: number, awayScore: number) {
-		this.homeScore = homeScore;
-		this.awayScore = awayScore;
+		this.#homeScore = homeScore;
+		this.#awayScore = awayScore;
 	}
 
 	get() {
 		return {
-			startTime: this.startTime,
-			homeTeam: this.homeTeam,
-			homeScore: this.homeScore,
-			awayTeam: this.awayTeam,
-			awayScore: this.awayScore,
+			startTime: this.#startTime,
+			homeTeam: this.#homeTeam,
+			homeScore: this.#homeScore,
+			awayTeam: this.#awayTeam,
+			awayScore: this.#awayScore,
 		};
 	}
 }
