f532eda649f114464bc957c2486ad227b72e3d71
undg <und3rdg@gmail.com>
2024-11-02 14:01:59 +0000

    jsdoc

--- src/match.ts
+++ src/match.ts
@@ -9,9 +9,14 @@ type Teams = [
 	/** away team */
 	Team,
 ];
+
 export class Match {
 	public startTime: Date;
 
+	/**
+	 * Creates a new Match instance.
+	 * @param {Teams} teams - Array containing home and away teams.
+	 */
 	constructor(teams: Teams) {
 		this.startTime = new Date();
 	}
